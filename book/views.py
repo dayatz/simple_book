ce68eb890b594ac78d7ad368061a050ac35658de
dayatz <dayat.py@gmail.com>
2016-03-08 15:18:26 +0800

    fix authentication class

--- book/views.py
+++ book/views.py
@@ -1,12 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.authtoken import TokenAuthentication
+from rest_framework.authentication import TokenAuthentication
 from .models import Book
 from .serializers import BookSerializer
 
 
 class BookViewSet(ModelViewSet):
     serializer_class = BookSerializer
-    permissions_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     authentication_classes = [TokenAuthentication]
     queryset = Book.objects.all()
